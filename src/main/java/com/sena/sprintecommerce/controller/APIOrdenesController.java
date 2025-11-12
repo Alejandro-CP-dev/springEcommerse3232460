fd936fe0fc57dacdb0dd7018d122d88bc2150b12
Alejandro-CP-dev <alejandrotareas029@gmail.com>
2025-11-12 17:40:50 -0500

    v0.7_claseAPIOrdenController_JhonCardenas_12112025

--- src/main/java/com/sena/sprintecommerce/controller/APIOrdenesController.java
+++ src/main/java/com/sena/sprintecommerce/controller/APIOrdenesController.java
@@ -1,5 +1,6 @@
 package com.sena.sprintecommerce.controller;
 
+import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 import java.util.Optional;
@@ -14,7 +15,9 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.sena.sprintecommerce.model.DetalleOrden;
 import com.sena.sprintecommerce.model.Orden;
+import com.sena.sprintecommerce.model.Producto;
 import com.sena.sprintecommerce.model.Usuario;
 import com.sena.sprintecommerce.service.IDetalleOrdenService;
 import com.sena.sprintecommerce.service.IOrdenService;
@@ -37,7 +40,13 @@ public class APIOrdenesController {
 	@Autowired
 	private IProductoService productoService;
 
-	// Endpoint GET para obtener todos los Ordenes
+	// Lista temporal de DetallesOrden
+	private List<DetalleOrden> detalleTemp = new ArrayList<>();
+
+	// Variable Temporal para la Orden
+	private Orden ordenTemp = new Orden();
+
+	// Endpoint GET para obtener todas los Ordenes
 	@GetMapping("/list")
 	public List<Orden> getALLOrdenes() {
 		return ordenService.findAll();
@@ -49,20 +58,72 @@ public class APIOrdenesController {
 		Optional<Orden> orden = ordenService.findById(id);
 		return orden.map(ResponseEntity::ok).orElse(ResponseEntity.notFound().build());
 	}
+	
+	// Endpoint GET para mostrar el detalle orden temporal
+		@GetMapping("/temporden")
+		public ResponseEntity<List<DetalleOrden>> verOrdenTemp() {
+			return ResponseEntity.ok(detalleTemp);
+		}
+	
+	// Endpoint POST para agregar productos temporales
+		@PostMapping("/agregar")
+		public ResponseEntity<List<DetalleOrden>> addProduct(@RequestBody DetalleOrden detalle) {
+			var producto = productoService.get(detalle.getProducto().getId())
+					.orElseThrow(() -> new RuntimeException("Producto no encontrado"));
+
+			// verificar si ya esta existiendo
+			boolean existe = false;
+			for (DetalleOrden d : detalleTemp) {
+				if (d.getProducto().getId().equals(producto.getId())) {
+					d.setCantidad(d.getCantidad() + detalle.getCantidad()); // Si ya existe, solo aumenta la cantidad
+					d.setTotal(d.getCantidad() * d.getPrecio()); // multiplica la cantidad por el precio
+					existe = true;
+					break;
+				}
+			}
+
+			if (!existe) {
+				detalle.setProducto(producto);
+				detalle.setPrecio(producto.getPrecio());
+				detalle.setTotal(producto.getPrecio() * detalle.getCantidad());
+				detalleTemp.add(detalle);
+			}
+
+			// Actualizar el total
+			double total = detalleTemp.stream().mapToDouble(DetalleOrden::getTotal).sum();
+			ordenTemp.setTotal(total);
+			return ResponseEntity.ok(detalleTemp);
+		}
 
 	// Endpoint POST para crear un nuevo producto
 	@PostMapping("/create")
 	public ResponseEntity<Orden> createOrden(@RequestBody Orden orden) {
-		Usuario u = usuarioService.findById(orden.getUsuario().getId())
-				.orElseThrow(() -> new RuntimeException("Usuario no encontrado"));
+		// Buscar el usuario
+		Usuario u = usuarioService.findById(1).get();
 		orden.setUsuario(u);
-		Date setFechaCreacion = new Date();
+		
 		orden.setTotal(0.0);
 		orden.setNumero(ordenService.generarNumeroOrden());
-		orden.setFechacreacion(setFechaCreacion);
-		orden.setUsuario(u);
+		orden.setFechacreacion(new Date());
+
 		Orden savedOrden = ordenService.save(orden);
+
+		// Guardar cada detalle y asociarlo a la orden
+		for (DetalleOrden d : detalleTemp) {
+			d.setOrden(savedOrden);
+			detalleService.save(d);
+
+			// Descontar stock de Producto
+			Producto producto = d.getProducto();
+			producto.setCantidad((int) (producto.getCantidad() - d.getCantidad()));
+			productoService.uptade(producto);
+		}
+
+		// Limpiar listas Temporales
+		detalleTemp.clear();
+		ordenTemp = new Orden();
+
 		return ResponseEntity.status(HttpStatus.CREATED).body(savedOrden);
 	}
-   
+
 }
